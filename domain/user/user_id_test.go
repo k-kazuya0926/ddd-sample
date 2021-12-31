f0e29f4b6b89ddd88f96b8b69a71c0fdda14e3a2
Kazuya Kobayashi <kobakazu3686@gmail.com>
2021-12-31 16:38:00 +0900

    テスト追加

---
+++ domain/user/user_id_test.go
@@ -0,0 +1,64 @@
+package user
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/oklog/ulid/v2"
+)
+
+func TestParseUserID(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    UserID
+		wantErr bool
+	}{
+		{
+			name: "正常系",
+			args: args{
+				id: "1234567890123456789012345A",
+			},
+			want: UserID{
+				id: ulid.MustParse("1234567890123456789012345A"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "異常系：25文字",
+			args: args{
+				id: "1234567890123456789012345",
+			},
+			wantErr: true,
+		},
+		{
+			name: "異常系：27文字",
+			args: args{
+				id: "123456789012345678901234567",
+			},
+			wantErr: true,
+		},
+		{
+			name: "異常系：空文字",
+			args: args{
+				id: "",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseUserID(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseUserID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseUserID() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
